83cb02289c70dabec03e2fe8a4a55329e5a536bd
Mariana <marianafloriano24@gmail.com>
2025-05-07 19:34:54 -0300

    :sparkles: funcionando

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -53,21 +53,41 @@ export class CourseComponent implements OnInit {
   salvar() {
     const dadosFormulario = {
       ...this.formGroupCurso.value,
-      disciplina: this.disciplinasSelecionadas // Associa as disciplinas selecionadas ao formulário
+      disciplina: this.disciplinasSelecionadas
     };
-
-    this.service.salvar(dadosFormulario).subscribe({
-      next: json => {
-        this.curso.push(json);
-        this.formGroupCurso.reset();
-        this.disciplinasSelecionadas = []; // Limpa as disciplinas selecionadas após salvar
-      }
-    });
+  
+    // Se estiver editando, faz o update
+    if (this.editando) {
+      this.service.atualizar(dadosFormulario).subscribe({
+        next: () => {
+          this.loadCurso();
+          this.formGroupCurso.reset();
+          this.disciplinasSelecionadas = [];
+          this.editando = null;
+        }
+      });
+    } else {
+      // Senão, faz o cadastro novo
+      this.service.salvar(dadosFormulario).subscribe({
+        next: json => {
+          this.curso.push(json);
+          this.formGroupCurso.reset();
+          this.disciplinasSelecionadas = [];
+        }
+      });
+    }
   }
+  
 
   remover(curso: curso) {
     this.service.delete(curso).subscribe({
       next: () => this.loadCurso()
     });
+  }
+
+  carregarParaEdicao(c: curso) {
+    this.editando = c.id; // Armazena o ID que está sendo editado
+    this.formGroupCurso.patchValue(c); // Preenche o formulário com os dados do curso
+    this.disciplinasSelecionadas = Array.isArray(c.disciplina) ? c.disciplina : [c.disciplina]; // Atualiza os checkboxes
   }
 }
